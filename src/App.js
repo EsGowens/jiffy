c6b7ed0cb9263d293de6abd390b5d5b5233bbe0a
esgowens <kinggsone@gmail.com>
2020-09-19 13:59:27 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./css/App.css";
+
+
+const Header = () => (
+  <div className='header grid'>
+    <h1 className='title'>Jiffy</h1>
+  </div>
+)
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
