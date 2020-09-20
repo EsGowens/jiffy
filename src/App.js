532742b7d340f5e329ef6e8fabd337715459cb9e
esgowens <kinggsone@gmail.com>
2020-09-20 16:12:56 +0000

    added site title and redirect with clear focus

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import loader from './images/loader.svg'
+import clearButton from './images/close-icon.svg'
 import Gif from './Gif'
 
 const randomChoice = arr => {
@@ -7,9 +8,17 @@ const randomChoice = arr => {
   return arr[randIndex];
 };
 
-const Header = () => (
+
+// when we have results we want to render close button
+const Header = ({clearSearch, hasResults}) => (
   <div className='header grid'>
-    <h1 className='title'>Jiffy</h1>
+  {hasResults ? ( 
+    <button>
+    <img src={clearButton} alt="clear search" onClick={clearSearch}/>
+    </button> 
+  ) : ( 
+      <h1 className='title'>Jiffy</h1>
+     )}
   </div>
 );
 
@@ -26,19 +35,28 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      loading: false,
       searchTerm: '',
       hintText: '',
-      gif: null,
       gifs: []
     };
   }
   
   searchGiphy = async searchTerm => {
+    this.setState({
+      // this is for the spinner
+      loading: true
+    })
     try {
       const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=l7bEpXZUWkG5UDMLEv84EXR6evq5chVR&q=${searchTerm}&limit=25&offset=0&rating=PG&lang=en`
     );
     // here we convert our raw response into json data
     const {data} = await response.json();
+    
+    // here we check if the arr is empty if it is we throw an error
+    if (!data.length) {
+      throw `Nothing found for ${searchTerm}`
+    }
     
     // here we grab a random result from our images
     const randomGif = randomChoice(data);
@@ -48,15 +66,21 @@ class App extends Component {
     
     this.setState((prevState, props) => ({
       ...prevState,
-      gif: randomGif,
       // we use our spread to take previous gif and add our new one on the end
-      gifs: [...prevState.gifs, randomGif]
+      gifs: [...prevState.gifs, randomGif],
+      // turns off the loading spinner after load
+      loading: false,
+      hintText: `Hit enter to see more ${searchTerm}`
     }));
     
     } catch (error) {
-      
+      this.setState((prevState, props) => ({
+        ...prevState,
+        hintText: error,
+        loading: false
+      }));
     }
-  }
+  };
   
   
   handleChange = event => { 
@@ -77,13 +101,25 @@ class App extends Component {
     }
   };
   
-  
+ // here we reset our state by clearing everything out bringing it to default
+  clearSearch = () => {
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: '',
+      hintText: '',
+      gifs: []
+    }));
+    this.textInput.focus()
+  }
   
 render() { 
-    const {searchTerm, gif} = this.state
+    const {searchTerm, gifs} = this.state;
+    // variable set to see if there are gifs
+    const hasResults = gifs.length;
     return (
       <div className="page">
-        <Header />
+        <Header clearSearch={this.clearSearch} hasResults={hasResults} />
+        
         <div className='search grid'>
         
         {/* here we loop over our arr of gif images from our state and create multiple videos from it */}
@@ -95,6 +131,9 @@ render() {
         onChange={this.handleChange}
         onKeyPress={this.handleKeyPress}
         value={searchTerm}
+        ref={input => {
+          this.textInput = input;
+        }}
         />
         </div>
         <UserHint {...this.state} />
